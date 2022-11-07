376316a1036043f827473fa558acc73062dd478e
Steven Bryant <stevelad_83@yahoo.com>
2022-11-07 18:30:30 -0500

    added more mushrooms

--- render-utils.js
+++ render-utils.js
@@ -1,6 +1,6 @@
 export function renderMushroom() {
     const div = document.createElement('div');
-    div.classList.add('mushroom');
+    div.classList.add('mushroom-porcini');
 
     return div;
 }
